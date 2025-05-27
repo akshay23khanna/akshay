073b1f2dd18efed1037592dd979bc52e80c03bd5
akshay23khanna <akshay232002khanna@gmail.com>
2025-05-27 16:18:14 +0530

    second commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String{}args){
+        system.out.println("hello world");
+                system.out.println("hello world again");
+                system.out.println("hello world again2");
+
+    }
+}
